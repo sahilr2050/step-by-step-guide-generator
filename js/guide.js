9515db02fef52df5130c24d5fb5ac232b76c03cc
Sahil Patel <sahil@cloudemy.com.au>
2025-02-26 02:09:00 +0530

    Add image blurring presets, sensitive info detection, and resume guide functionality

--- js/guide.js
+++ js/guide.js
@@ -1,4 +1,5 @@
 import Core from './guide/core.js';
+import '../libs/bootstrap.bundle.min.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   Core.init();
--- js/guide/store.js
+++ js/guide/store.js
@@ -1,36 +1,39 @@
 // store.js
 export default {
-    // DOM elements
-    guideTitle: null,
-    guideContent: null,
-    imageModal: null,
-    modalImage: null,
-    modalClose: null,
-    addBlurBtn: null,
-    removeBlurBtn: null,
-    saveBlurredImageBtn: null,
-    
-    // State variables
-    currentGuide: null,
-    guideId: null,
-    currentStepIndex: null,
-    blurRegions: [],
-    isDragging: false,
-    isResizing: false,
-    currentBlurRegion: null,
-    startX: 0, startY: 0,
-    startWidth: 0, startHeight: 0,
-    originalImage: null,
-    draggedStep: null,
-  
-    initElements() {
-      this.guideTitle = document.getElementById('guide-title');
-      this.guideContent = document.getElementById('guide-content');
-      this.imageModal = document.getElementById('imageModal');
-      this.modalImage = document.getElementById('modalImage');
-      this.modalClose = document.querySelector('.modal-close');
-      this.addBlurBtn = document.getElementById('addBlurRegion');
-      this.removeBlurBtn = document.getElementById('removeBlurRegion');
-      this.saveBlurredImageBtn = document.getElementById('saveBlurredImage');
-    }
-  };
+  // DOM elements
+  guideTitle: null,
+  guideContent: null,
+  imageModal: null,
+  modalImage: null,
+  modalClose: null,
+  addBlurBtn: null,
+  removeBlurBtn: null,
+  saveBlurredImageBtn: null,
+
+  // State variables
+  currentGuide: null,
+  guideId: null,
+  currentStepIndex: null,
+  blurRegions: [],
+  isDragging: false,
+  isResizing: false,
+  currentBlurRegion: null,
+  startX: 0, startY: 0,
+  startWidth: 0, startHeight: 0,
+  originalImage: null,
+  draggedStep: null,
+
+  initElements() {
+    this.guideTitle = document.getElementById('guide-title');
+    this.guideContent = document.getElementById('guide-content');
+    this.imageModal = document.getElementById('imageModal');
+    this.modalImage = document.getElementById('modalImage');
+    this.modalClose = document.querySelector('.modal-close');
+    this.addBlurBtn = document.getElementById('addBlurRegion');
+    this.removeBlurBtn = document.getElementById('removeBlurRegion');
+    this.saveBlurredImageBtn = document.getElementById('saveBlurredImage');
+    this.detectSensitiveBtn = document.getElementById('detectSensitiveInfo');
+    this.blurPresetsBtn = document.getElementById('blurPresets');
+
+  }
+};
